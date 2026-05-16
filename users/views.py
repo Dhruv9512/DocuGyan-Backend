bab2a3116efe2d1a4735c6fd20f6b24c16cc4b0e
Dhruv Sharma <dhruvsharma56780@gmail.com>
2026-05-16 09:32:53 +0530

    Squash changes

--- users/views.py
+++ users/views.py
@@ -34,7 +34,8 @@ def jwt_cookie_opts():
     return {
         "httponly": True,
         "secure": True,
-        "samesite": "None",
+        "samesite": "Lax",
+        "path": "/",
     }
 
 def set_auth_cookies(response: Response, access_token: str, refresh_token: str):
@@ -43,8 +44,8 @@ def set_auth_cookies(response: Response, access_token: str, refresh_token: str):
     response.set_cookie(REFRESH_COOKIE, refresh_token, max_age=REFRESH_MAX_AGE, **opts)
 
 def clear_auth_cookies(response: Response):
-    response.delete_cookie(ACCESS_COOKIE)
-    response.delete_cookie(REFRESH_COOKIE)
+    response.delete_cookie(ACCESS_COOKIE, path='/')
+    response.delete_cookie(REFRESH_COOKIE, path='/')
 
 def issue_tokens_for_user(user: CustomUser):
     refresh = RefreshToken.for_user(user)
